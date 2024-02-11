a0c8fdc3548a0daa83df944e24ddd076bf0dea86
grasuggan <lena_reo@hotmail.com>
2024-02-11 12:32:10 +0100

    carousel

--- src/index.js
+++ src/index.js
@@ -16,3 +16,28 @@ $(function () {
     }
   });
 });
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  $(".scroller").each(function () {
+    // add data-animated="true" to every `.scroller` on the page
+    $(this).attr("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = $(this).find(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children());
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach(function (item) {
+      const duplicatedItem = $(item).clone(true);
+      duplicatedItem.attr("aria-hidden", true);
+      scrollerInner.append(duplicatedItem);
+    });
+  });
+}
